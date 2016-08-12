8da52021c8972f1c9bf7ed65828b1a1eb0c7f047
John Wang <johncwang@gmail.com>
2016-08-12 01:09:37 -0700

    update tests

--- test/test_setup_oauth.rb
+++ test/test_setup_oauth.rb
@@ -1,6 +1,7 @@
 require './test/test_base.rb'
 
 require 'faraday'
+require 'multi_json'
 require 'oauth2'
 
 class MediumSdkSetupOAuthTest < Test::Unit::TestCase
@@ -14,12 +15,30 @@ class MediumSdkSetupOAuthTest < Test::Unit::TestCase
     assert_equal 'dead', @sdk.connection.client_id
     assert_equal @redirect_uri, @sdk.connection.oauth_redirect_uri
     assert_equal 'https://api.medium.com/v1', @sdk.connection.api_version_uri
+  end
 
+  def token_hash
     token = {
       'access_token' => @string
     }
     @sdk.connection.set_token token
     token2 = @sdk.connection.token.to_hash
     assert_equal @string, token2[:access_token]
+  end
+
+  def token_string_json
+    token = {
+      'access_token' => @string + @string
+    }
+    token2 = MultiJson.encode token
+    @sdk.connection.set_token token2
+    token3 = @sdk.connection.token.to_hash
+    assert_equal (@string + @string), token3[:access_token]  
+  end
+
+  def token_string
+    @sdk.connection.set_token @string
+    token = @sdk.connection.token.to_hash
+    assert_equal @string, token[:access_token]  
   end
 end
