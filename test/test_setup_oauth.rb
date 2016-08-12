edbc4b424103aaa26a0fcccbeea517c51e7cef7d
John Wang <johncwang@gmail.com>
2016-08-12 01:16:05 -0700

    update tests

--- test/test_setup_oauth.rb
+++ test/test_setup_oauth.rb
@@ -15,18 +15,14 @@ class MediumSdkSetupOAuthTest < Test::Unit::TestCase
     assert_equal 'dead', @sdk.connection.client_id
     assert_equal @redirect_uri, @sdk.connection.oauth_redirect_uri
     assert_equal 'https://api.medium.com/v1', @sdk.connection.api_version_uri
-  end
 
-  def token_hash
     token = {
       'access_token' => @string
     }
     @sdk.connection.set_token token
     token2 = @sdk.connection.token.to_hash
     assert_equal @string, token2[:access_token]
-  end
 
-  def token_string_json
     token = {
       'access_token' => @string + @string
     }
@@ -34,9 +30,7 @@ class MediumSdkSetupOAuthTest < Test::Unit::TestCase
     @sdk.connection.set_token token2
     token3 = @sdk.connection.token.to_hash
     assert_equal (@string + @string), token3[:access_token]  
-  end
 
-  def token_string
     @sdk.connection.set_token @string
     token = @sdk.connection.token.to_hash
     assert_equal @string, token[:access_token]  
