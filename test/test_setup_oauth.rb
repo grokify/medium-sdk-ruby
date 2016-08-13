e6a0face29015ad9db44e38bffcadd3eed7ed3b1
John Wang <johncwang@gmail.com>
2016-08-12 23:43:32 -0700

    update tests

--- test/test_setup_oauth.rb
+++ test/test_setup_oauth.rb
@@ -9,6 +9,7 @@ class MediumSdkSetupOAuthTest < Test::Unit::TestCase
     @string = 'deadbeef'
     @redirect_uri = 'https://example.com/callback'
     @sdk = MediumSdk.new client_id: 'dead', client_secret: 'beef', redirect_uri: @redirect_uri
+    @sdk2 = MediumSdk.new client_id: 'dead', client_secret: 'beef', redirect_uri: @redirect_uri
   end
 
   def test_main
@@ -33,6 +34,13 @@ class MediumSdkSetupOAuthTest < Test::Unit::TestCase
 
     @sdk.connection.set_token @string
     token = @sdk.connection.token.to_hash
-    assert_equal @string, token[:access_token]  
+    assert_equal @string, token[:access_token]
+
+    auth_uri = @sdk.connection.authorize_uri
+    assert_equal 'https://medium.com/m/oauth/authorize?client_id=dead&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code', auth_uri
+
+    assert_raise do 
+      @sdk2.connection.set_token(['fail'])
+    end
   end
 end
